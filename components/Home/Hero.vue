55b695d29080970c1a02b408646bfe88cf686b90
anya <riidloa@gmail.com>
2023-08-31 08:59:22 +0700

    add login and register page

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -28,9 +28,22 @@
             </h4>
             <NuxtLink to="/login"
               ><ButtonOutline :class="'mt-8'"
-                >Start Whisping Now ➔</ButtonOutline
-              ></NuxtLink
-            >
+                >Start Whisping Now
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6 inline-block"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                  />
+                </svg> </ButtonOutline
+            ></NuxtLink>
           </div>
           <div
             class="right flex justify-center"
--- pages/register.vue
+++ pages/register.vue
@@ -41,16 +41,27 @@
       class="card w-full sm:w-96 text-neutral-content px-2 py-1 backdrop-blur-3xl bg-indigo-800/40 rounded-none md:rounded-md"
     >
       <div class="card-body items-center text-center">
-        <div class="card-title text-center mb-4">Login to Your Account</div>
+        <div class="card-title text-center mb-4">Create a New Account</div>
         <div class="form-control w-full max-w-xs">
+          <div class="mb-4">
+            <label class="label">
+              <span class="label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              class="text-sm font-mono input input-bordered w-full max-w-xs text-white focus:border-2 focus:border-violet-500 rounded-md"
+            />
+          </div>
+          <div class="mb-4">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              class="text-sm font-mono input input-bordered w-full max-w-xs text-white focus:border-2 focus:border-violet-500 rounded-md"
+            />
+          </div>
           <label class="label">
-            <span class="label-text">Email/Username</span>
-          </label>
-          <input
-            type="text"
-            class="text-sm font-mono input input-bordered w-full max-w-xs text-white focus:border-2 focus:border-violet-500 rounded-md"
-          />
-          <label class="label mt-4">
             <span class="label-text">Password</span>
           </label>
           <input
@@ -63,7 +74,7 @@
             class="btn rounded-md btn-primary w-full"
             @click="message = 'Your password is incorrect!'"
           >
-            Login
+            Register
           </button>
         </div>
         <div class="divider"></div>
@@ -82,13 +93,13 @@
               d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
             />
           </svg>
-          <span class="flex-grow">Login with Google</span>
+          <span class="flex-grow">Register with Google</span>
         </button>
         <span class="mx-auto w-full mt-4 text-sm">
           <NuxtLink
-            to="/register"
+            to="/login"
             :class="'underline underline-offset-4 hover:text-violet-700'"
-            >sign up</NuxtLink
+            >login</NuxtLink
           >
           instead</span
         >
